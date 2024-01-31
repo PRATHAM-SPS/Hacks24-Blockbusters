ddedb32be72636c9f2d5389bf3d65d2681083355
OnkarSuryavanshi <suryavanshionkar2002@gmail.com>
2024-01-31 18:18:51 +0530

    signup ui

--- Blockchain/src/Components/registration/signup.jsx
+++ Blockchain/src/Components/registration/signup.jsx
@@ -46,29 +46,125 @@ function Signup() {
 
     return (
         <div className="login">
-
-            <h1>Signup</h1>
-
-            <form action="POST">
-                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
-                <input type="name" onChange={(e) => { setName(e.target.value) }} placeholder="Name" />
-                <input type="age" onChange={(e) => { setAge(e.target.value) }} placeholder="Age" />
-                <input type="address" onChange={(e) => { setAddress(e.target.value) }} placeholder="Address" />
-                <input type="pin" onChange={(e) => { setPin(e.target.value) }} placeholder="Pin Code Number" />
-                <input type="pan" onChange={(e) => { setPan(e.target.value) }} placeholder="Pan Number" />
-                <input type="adhar" onChange={(e) => { setAdhar(e.target.value) }} placeholder="Adhar" />
-                <input type="submit" onClick={submit} />
-
-            </form>
-
-            <br />
-            <p>OR</p>
-            <br />
-
-            <Link to="/">Login Page</Link>
-
+<div className="container-xxl py-5 predict">
+  <div className="container">
+    <div
+      className="text-center mx-auto mb-5 wow fadeInUp"
+      data-wow-delay="0.1s"
+      style={{ maxWidth: "600px" }}
+    >
+      <h1 className="mb-3">Signup</h1>
+      <p>
+        Ready to join us? Fill out the form below to create your account.
+      </p>
+    </div>
+    <div className="row g-4">
+      <div className="col-12">
+        <div className="row gy-4">
+          <div className="col-md-6" style={{ width: "100%" }}>
+            <div className="wow fadeInUp" data-wow-delay="0.5s">
+              <form action="POST">
+                <div className="row g-3">
+                  <div className="col-12">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="Email"
+                      onChange={(e) => {setEmail(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Password"
+                      onChange={(e) => {setPassword(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="name"
+                      placeholder="Name"
+                      onChange={(e) => {setName(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="age"
+                      placeholder="Age"
+                      onChange={(e) => {setAge(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      placeholder="Address"
+                      onChange={(e) => {setAddress(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="pin"
+                      placeholder="Pin Code Number"
+                      onChange={(e) => {setPin(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="pan"
+                      placeholder="Pan Number"
+                      onChange={(e) => {setPan(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="adhar"
+                      placeholder="Adhar"
+                      onChange={(e) => {setAdhar(e.target.value)}}
+                    />
+                  </div>
+                  <div className="col-12">
+                    <button
+                      className="btn btn-primary w-100 py-3"
+                      type="submit"
+                      onClick={submit}
+                    >
+                      Signup
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          
+          <div className="col-md-6" style={{ width: "100%" }}>
+            <div className="wow fadeInUp" data-wow-delay="0.5s">
+              <br />
+              <p>OR</p>
+              <br />
+              <Link to="/">Login Page</Link>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
     )
 }
 
